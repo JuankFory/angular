2edc3f0c9947808b41baec4dfa227f48bf733ed0
Juank <juankfory4@gmail.com>
2022-08-08 17:58:27 -0500

    prueba tecnica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { InicioComponent } from './vista/inicio/inicio.component';
+import { NuevoComponent } from './vista/nuevo/nuevo.component';
+import { EditarComponent } from './vista/editar/editar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', redirectTo:'inicio', pathMatch:'full'},
+{path:'inicio', component:InicioComponent},
+{path:'nuevo', component:NuevoComponent},
+{path:'editar',component:EditarComponent}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './vista/inicio/inicio.component';
+import { NuevoComponent } from './vista/nuevo/nuevo.component';
+import { EditarComponent } from './vista/editar/editar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    NuevoComponent,
+    EditarComponent
   ],
   imports: [
     BrowserModule,
